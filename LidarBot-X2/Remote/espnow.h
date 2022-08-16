f976164a3179748e550550cc57f7cbae320b4c56
TinyuChiu <TInyu.Zhao@gmail.com>
2022-08-16 09:44:14 +0800

    Fix compilation errors

--- LidarBot-X2/Remote/espnow.h
+++ LidarBot-X2/Remote/espnow.h
@@ -5,74 +5,88 @@
 #include <Preferences.h>
 #include <WiFi.h>
 
-#define SLAVE_CHANNEL 1
-#define CHANNEL 3
+#define SLAVE_CHANNEL    1
+#define CHANNEL          1
 #define PRINTSCANRESULTS 0
 #define DELETEBEFOREPAIR 0
 
-typedef void (*recvCB)(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+typedef void (*recvCB)(const uint8_t *mac_addr, const uint8_t *data,
+                       int data_len);
 typedef void (*sendCB)(const uint8_t *, esp_now_send_status_t);
 
 class EspNow {
-public:
-	static esp_now_peer_info_t slave;
+   public:
+    static esp_now_peer_info_t slave;
 
-	EspNow();
-	~EspNow();
-	static void sendData(void *buff, int len);
-	void deletePeer(void);
-	bool manageSlave(void);
+    EspNow();
+    ~EspNow();
+    static void sendData(void *buff, int len);
+    void deletePeer(void);
+    bool manageSlave(void);
 };
 
 struct peerList {
-	esp_now_peer_info_t list[40];
-	uint8_t count = 0;
+    esp_now_peer_info_t list[40];
+    uint8_t count = 0;
 };
 
 class EspNowMaster : public EspNow {
-public:
-	bool isConnected = false;
-	static peerList peerlist;
-	static recvCB recvCallBack;
-	static sendCB sendCallBack;
+   public:
+    bool isConnected = false;
+    static peerList peerlist;
+    static recvCB recvCallBack;
+    static sendCB sendCallBack;
 
-	EspNowMaster();
-	~EspNowMaster();
-	static int8_t isPeerExist(esp_now_peer_info_t peer_info);
-	static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-	static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len);
-	void setRecvCallBack(recvCB cb) {recvCallBack = cb;};
-	void setSendCallBack(sendCB cb) {sendCallBack = cb;};
-	void InitBroadcastSlave(void);
-	bool confirmPeer(esp_now_peer_info_t peer);
-	void Broadcast();
-	void Init();
+    EspNowMaster();
+    ~EspNowMaster();
+    static int8_t isPeerExist(esp_now_peer_info_t peer_info);
+    static void OnDataSent(const uint8_t *mac_addr,
+                           esp_now_send_status_t status);
+    static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data,
+                           int data_len);
+    void setRecvCallBack(recvCB cb) {
+        recvCallBack = cb;
+    };
+    void setSendCallBack(sendCB cb) {
+        sendCallBack = cb;
+    };
+    void InitBroadcastSlave(void);
+    bool confirmPeer(esp_now_peer_info_t peer);
+    void Broadcast();
+    void Init();
 
-private:
-	Preferences preferences;
+   private:
+    Preferences preferences;
 };
 
 class EspNowSlave : public EspNow {
-public:
-	bool isConnected = false;
-	static peerList peerlist;
-	// static void (*recvCallBack)(const uint8_t *mac_addr, const uint8_t *data, int data_len);
-	static recvCB recvCallBack;
-	static sendCB sendCallBack;
+   public:
+    bool isConnected = false;
+    static peerList peerlist;
+    // static void (*recvCallBack)(const uint8_t *mac_addr, const uint8_t *data,
+    // int data_len);
+    static recvCB recvCallBack;
+    static sendCB sendCallBack;
 
-	EspNowSlave();
-	~EspNowSlave();
-	void Ack(esp_now_peer_info_t peer);
-	void Init();
-	void configDeviceAP();
-	static int8_t isPeerExist(esp_now_peer_info_t peer);
-	static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-	static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len);
-	void setRecvCallBack(recvCB cb) {recvCallBack = cb;};
-	void setSendCallBack(sendCB cb) {sendCallBack = cb;};
+    EspNowSlave();
+    ~EspNowSlave();
+    void Ack(esp_now_peer_info_t peer);
+    void Init();
+    void configDeviceAP();
+    static int8_t isPeerExist(esp_now_peer_info_t peer);
+    static void OnDataSent(const uint8_t *mac_addr,
+                           esp_now_send_status_t status);
+    static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data,
+                           int data_len);
+    void setRecvCallBack(recvCB cb) {
+        recvCallBack = cb;
+    };
+    void setSendCallBack(sendCB cb) {
+        sendCallBack = cb;
+    };
 
-private:
-	Preferences preferences;
+   private:
+    Preferences preferences;
 };
 
 #endif
